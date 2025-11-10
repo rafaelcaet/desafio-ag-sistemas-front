960d7217439e5160a64326a6776682d31a230ce8
rafaelcaet <fael.caetsantos@gmail.com>
2025-11-10 17:42:55 -0300

    feat: add user table component and users page

--- src/_components/AppSidebar.tsx
+++ src/_components/AppSidebar.tsx
@@ -6,7 +6,7 @@ import {
     SidebarHeader,
   } from "@/components/ui/sidebar"
 import Link from "next/link";
-import { ChartPieIcon, GroupIcon, HomeIcon, UsersIcon } from "lucide-react";
+import { ChartPieIcon, HomeIcon, UsersIcon } from "lucide-react";
 
 const sidebarItems = [
   {
@@ -17,7 +17,7 @@ const sidebarItems = [
   {
     label: "Usuários",
     icon: <UsersIcon size={24}/>,
-    href: "/users",
+    href: "/usuarios",
   },
   {
     label: "Dashboard",
@@ -32,12 +32,13 @@ const SidebarItem = ({ label, icon, href }: { label: string, icon: React.ReactNo
       <div className="flex items-center text-[#13679F] justify-start
        rounded-md hover:bg-[#13679F] py-2 px-1 hover:text-slate-100 gap-6"
        title={label}>
-        <span>{icon}</span>
+        <span className="w-6 h-6">{icon}</span>
         <span className="text-sm font-medium">{label}</span>
       </div>
     </Link>
   );
 };
+
 export function AppSidebar() {
   return (
     <Sidebar className="border" variant="sidebar" collapsible="icon">
---
+++ src/app/(private)/usuarios/page.tsx
@@ -0,0 +1,94 @@
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHeader, TableRow } from "@/components/ui/table";
+
+const leadsIndication = [
+    {
+        _id: "1",
+        name: "John Doe",
+        phone: "1234567890",
+        createdAt: "2021-01-01",
+    },
+    {
+        _id: "2",
+        name: "John Smith",
+        phone: "1234567890",
+        createdAt: "2021-01-01",
+    },
+    {
+        _id: "3",
+        name: "John Pires",
+        phone: "1234567890",
+        createdAt: "2021-01-01",
+    },
+    {
+        _id: "4",
+        name: "John Silva",
+        phone: "1234567890",
+        createdAt: "2021-01-01",
+    },
+]
+
+export default function Usuarios() {
+    return (
+        <div className="relative">
+              <Table className="w-[70%] mx-auto rounded-lg mt-8 text-[#13679F]">
+                <TableHeader className="border-gray-100 dark:border-gray-800 border-y">
+                  <TableRow>
+                    <TableCell className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400">
+                      Nome
+                    </TableCell>
+                    <TableCell                     
+                      className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400 hidden md:table-cell"
+                    >
+                      Telefone
+                    </TableCell>
+                    <TableCell
+                      className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400 hidden md:table-cell"
+                    >
+                      Data de cadastro
+                    </TableCell>
+                    <TableCell  className="py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400">
+                      Ações
+                    </TableCell>
+                  </TableRow>
+                </TableHeader>
+
+                <TableBody className="divide-y divide-gray-100 dark:divide-gray-800">
+                  {leadsIndication.map((lead: any) => (
+                    <TableRow key={lead._id}>
+                      <TableCell className="py-3">
+                        <div className="flex items-center gap-3">
+                            {lead.name}
+                        </div>
+                      </TableCell>
+
+                      <TableCell
+                        className="py-3 text-gray-500 text-theme-sm dark:text-gray-400 whitespace-nowrap hidden md:table-cell">
+                            {lead.phone}
+                      </TableCell>
+
+                      <TableCell
+                        className="py-3 text-gray-500 text-theme-sm dark:text-gray-400 whitespace-nowrap hidden md:table-cell">
+                        {lead.createdAt}
+                      </TableCell>
+
+                      <TableCell className="flex py-3 text-gray-500 text-theme-sm gap-4 dark:text-gray-400 whitespace-nowrap">
+                        <Button
+                          className="text-gray-500 border-gray-300 dark:border-gray-700"
+                        >
+                          Ver Dados
+                        </Button>
+                          <Button
+                            variant="outline"
+                            className="text-gray-500 border-gray-300 dark:border-gray-700"
+                          >
+                            Editar
+                          </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+    )
+}
